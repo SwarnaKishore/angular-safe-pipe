5061ee70529a6e0997812e1561a3309efbbaff86
Swarna <swarnakishoree@gmail.com>
2017-08-10 16:40:00 -0500

    updated styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer } from '@angular/core';
  declare let swal: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  htmlSnippet: string = '<script>swal("Good job!", "You clicked the button!", "success")</script>';
+  htmlSnippet: string = '<script>swal("Hi !, I am script and I bypassed angular security", "", "success")</script>';
+  @ViewChild('element') public viewElement: ElementRef;
+  public element: any;
+
+  constructor(public renderer: Renderer) {}
+
   public ngOnInit()
   {
-    
+   this.appendHTMLSnippetToDOM();
+  }
+  public appendHTMLSnippetToDOM()
+  {
+    this.element = this.viewElement.nativeElement;
+    const fragment = document.createRange().createContextualFragment(this.htmlSnippet);
+    this.element.appendChild(fragment);
   }
 }
